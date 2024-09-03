2c6d82f6cd941896277739360066f9787d4fac58
andretalyss1 <andretalyss1@gmail.com>
2024-09-02 21:33:26 -0300

    Adc transform function

--- correletion/main.py
+++ correletion/main.py
@@ -3,8 +3,15 @@ from PIL import Image
 import cv2
 
 image_path = 'images/arara.jpeg'
-filtro_path = 'filtros/filtro-gauss.txt'
-output_image_name = 'output/image-correletion-gauss.png'
+filtro_path = 'filtros/filtro-media.txt'
+output_image_name = 'output/image-correletion-media.png'
+
+def transform(matriz, m, n):
+    rows, cols = matriz.shape
+    matriz_res = np.zeros((m,n))
+    matriz_res[:rows, :cols] = matriz
+
+    return matriz_res
 
 def read_filter_file(file):
     filtro = []
@@ -19,6 +26,7 @@ def read_filter_file(file):
 
     return m,n, offset, filtro
 
+
 def correlacao(image, m, n, filtro):
     imagem = cv2.imread(image)
 
@@ -30,45 +38,29 @@ def correlacao(image, m, n, filtro):
     img_matriz = imagem
 
     print(f"Imagem original {height}x{width}")
-
-    b,g,r = cv2.split(img_matriz)
+                                                                   
+    b,g,r = cv2.split(img_matriz)                              
     matriz_aux = np.zeros((m,n), dtype=float)
     
-    for x in range(height-1):
-        for y in range(width-1):
-            rows, cols = r[x:x+m, y:y+n].shape
-            pad_top = (m - rows) // 2
-            pad_bottom = m - rows - pad_top
-            pad_left = (n - cols) // 2
-            pad_right = n - cols - pad_left
-
-            r_padded = np.pad(r[x:x+m, y:y+n], ((pad_top, pad_bottom), (pad_left, pad_right)), mode='constant', constant_values=0)
+    for x in range(height):
+        for y in range(width):
+            r_padded = transform(r[x:x+m, y:y+n], m, n)
             matriz_aux[:,:] = r_padded
             matriz_aux =  matriz_aux * filtro
             pixel_r = (np.sum(matriz_aux))
 
-            rows, cols = g[x:x+m, y:y+n].shape
-            pad_top = (m - rows) // 2
-            pad_bottom = m - rows - pad_top
-            pad_left = (n - cols) // 2
-            pad_right = n - cols - pad_left
-
-            g_padded = np.pad(g[x:x+m, y:y+n], ((pad_top, pad_bottom), (pad_left, pad_right)), mode='constant', constant_values=0)
+            g_padded = transform(g[x:x+m, y:y+n], m, n)
             matriz_aux[:,:] = g_padded
             matriz_aux = matriz_aux * filtro
             pixel_g = (np.sum(matriz_aux))
 
-            rows, cols = b[x:x+m, y:y+n].shape
-            pad_top = (m - rows) // 2
-            pad_bottom = m - rows - pad_top
-            pad_left = (n - cols) // 2
-            pad_right = n - cols - pad_left
-
-            b_padded = np.pad(b[x:x+m, y:y+n], ((pad_top, pad_bottom), (pad_left, pad_right)), mode='constant', constant_values=0)
+            b_padded = transform(b[x:x+m, y:y+n], m, n)
             matriz_aux[:,:] = b_padded
             matriz_aux = matriz_aux * filtro
             pixel_b = (np.sum(matriz_aux))
 
+            if pixel_b <= 30 and pixel_g <= 30 and pixel_r  <= 30:
+                print(f"RGB = ({pixel_r}, {pixel_g}, {pixel_b})")
             img_matriz[x][y] = pixel_r, pixel_g, pixel_b
         
     filtered_image = Image.fromarray(img_matriz)
